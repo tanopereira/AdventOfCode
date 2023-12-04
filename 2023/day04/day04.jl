78d87b63464ca9296243443e639ccb159f8b7804
Gustavo Pereira <tanopereira@gmail.com>
2023-12-05 00:46:17 +0100

    add common solution

--- 2023/day04/day04.jl
+++ 2023/day04/day04.jl
@@ -35,3 +35,23 @@ end
 
 @time p2=part2(input)
 println("Part2: $p2")
+
+#Knowing how P1 and P2 need the same parsing
+function solution(input)
+    s=0
+    v=repeat([1],length(input))
+    for (i,line) in enumerate(input)
+        card,numbers=split(line,": ")
+        winning,holding=split(numbers,"|")
+        n1=parse.(Int,[m.match for m in eachmatch(r"\d+",winning)])
+        n2=parse.(Int,[m.match for m in eachmatch(r"\d+",holding)])
+        l=length(intersect(n1,n2))
+        if l>0
+            s+=2^(l-1)
+            v[i+1:i+l].+=v[i]
+        end
+    end
+    return s,sum(v)
+end
+
+@time p3,p4=solution(input)
